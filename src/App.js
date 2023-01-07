ff976e4fca61654feb74baa290ee5cd8759c5763
Ruan Noleto <84647446+ruannysil@users.noreply.github.com>
2023-01-07 12:53:49 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+
+// const menu = document.getElementById('menu');
+// const navMenu = document.querySelector('nav-menu');
+// menu.addEventListener('click', () => {
+//   menu.classList.toggle('active');
+//   navMenu.classList.toggle('active');
+// });
+
+
+// function mudar() {
+//   // const menu = document.getElementById('menu');
+//   const navMenu = document.querySelector('nav-menu');
+  
+//   if(navMenu.classList.toggle('active')) { 
+//     console.log('deu certo')
+//   } else {
+//     console.log('nao deu certo');
+//   }
+// }
+// mudar();
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <nav className="navigation">
+          <a href='#/' className='logo'>Ruanny<span>Noleto</span></a>
+          <ul id='menu' className='nav-menu'>
+            <li><a href='#/'>inicio</a></li>
+            <li><a href='#/'>sobre</a></li>
+            <li><a href='#/'>conhecimento</a></li>
+            <li><a href='#/'>experiencia</a></li>
+            <li><a href='#/'>educação</a></li>
+            <li><a href='#/'>contato</a></li>
+          </ul>
+          <div className='menu'>
+            <span className='bar'></span>
+            <span className='bar'></span>
+            <span className='bar'></span>
+          </div>
+        </nav>
       </header>
+      <main>
+        <section className='home'>
+          <div className='home-text'>
+            <h4 className='text-h4'>Welcome Developer</h4>
+            <h1 className='text-h1'>lorem lorem</h1>
+            <p>lorem
+            </p>
+          </div>
+          <div className='img'>
+            <img src='https://www.nationsrestaurants.com/wp-content/uploads/2022/06/hires_Nations_Hero_CheeseBurger-min.png' alt='img' />
+          </div>
+        </section>
+      </main>
     </div>
   );
 }

d68851f88daf1960fcf69d572e36d4cb4467517e
Ruan Noleto <84647446+ruannysil@users.noreply.github.com>
2023-01-15 13:59:09 -0300

    :dizzy: ongoing project, organized the portfolio, and api, and images  :100:

--- src/components/experience/Experience.js
+++ src/components/experience/Experience.js
@@ -16,67 +16,67 @@ export default function Experience() {
     {
       id: 1,
       descricao: 'acordion',
-      imgScr: 'https://user-images.githubusercontent.com/84647446/212429666-c816bfec-d9ad-4d93-a045-5dfdf3ca65d7.jpg',
+      imgScr: 'https://user-images.githubusercontent.com/84647446/212552072-feca1aaa-fa31-47d5-b7d5-175181486b6f.jpg',
     },
     {
       id: 2,
       descricao: 'calendario Copa',
-      imgScr: 'https://user-images.githubusercontent.com/84647446/212438484-5232a320-ef07-486c-8529-1414bddb6191.jpg',
+      imgScr: 'https://user-images.githubusercontent.com/84647446/212552074-0f7d696f-6370-486d-bb6c-4511aba14707.jpg',
     },
     {
       id: 3,
       descricao: 'Costs financial',
-      imgScr: 'https://user-images.githubusercontent.com/84647446/212437772-bc315011-e2dd-4105-be9f-2d6355179e5e.jpg',
+      imgScr: 'https://user-images.githubusercontent.com/84647446/212552075-68065107-f012-45d9-87c0-3b1a4384734b.jpg',
     },
     {
       id: 4,
       descricao: 'devflix #1',
-      imgScr: 'https://user-images.githubusercontent.com/84647446/212435190-0b934dbe-4a7b-4d67-9873-8a90d508a761.jpg',
+      imgScr: 'https://user-images.githubusercontent.com/84647446/212552078-675653f7-7139-40c1-b412-f903f31d6f4b.jpg',
     },
     {
       id: 5,
       descricao: 'game jokenpo',
-      imgScr: 'https://user-images.githubusercontent.com/84647446/212434460-b5d621ff-9852-4de9-b430-dc6c3aabb830.jpg',
+      imgScr: 'https://user-images.githubusercontent.com/84647446/212552082-51ac4244-121c-4207-9241-76537716ca37.jpg',
     },
     {
       id: 6,
       descricao: 'recuperando dados do github',
-      imgScr: 'https://user-images.githubusercontent.com/84647446/212432624-c8914645-db57-4153-9366-4c1b81dc601c.jpg',
+      imgScr: 'https://user-images.githubusercontent.com/84647446/212552092-7ba3553e-1bde-4572-a248-d10bee477c17.jpg',
     },
     {
       id: 7,
       descricao: 'tela login zé delivery',
-      imgScr: 'https://user-images.githubusercontent.com/84647446/212433817-08f5e4a8-0e3d-4430-abf0-178551e4596d.jpg'
+      imgScr: 'https://user-images.githubusercontent.com/84647446/212552096-c46b20b4-fb6d-4f32-a9c1-b1c3f73c0a52.jpg'
     },
     {
       id: 8,
       descricao: 'Loja virtual Games',
-      imgScr: 'https://user-images.githubusercontent.com/84647446/212438021-d33fcaf9-b706-4f92-88f5-af2b76257e7e.jpg',
+      imgScr: 'https://user-images.githubusercontent.com/84647446/212552085-c6d0c48a-5944-483f-9bb4-89502747e2ca.jpg',
     },
     {
       id: 9,
       descricao: 'Frases de Amor',
-      imgScr: 'https://user-images.githubusercontent.com/84647446/212436968-c7ea12b4-0152-433d-a730-d0b7c17573d7.jpg',
+      imgScr: 'https://user-images.githubusercontent.com/84647446/212552081-7c31f73f-fbf8-45e5-b2f4-300954422a62.jpg',
     },
     {
       id: 10,
       descricao: 'movimental filmes devflix #2',
-      imgScr: 'https://user-images.githubusercontent.com/84647446/212435868-ed019c81-9455-43f0-8503-3b22bc82eb6d.jpg',
+      imgScr: 'https://user-images.githubusercontent.com/84647446/212552087-7567fdfb-9ba0-49e9-92f3-f80bfd6eb4c0.jpg',
     },
     {
       id: 11,
       descricao: 'Nutridev',
-      imgScr: 'https://user-images.githubusercontent.com/84647446/212436071-a314dc31-7153-44d2-9fbe-bc3403d21b4a.jpg',
+      imgScr: 'https://user-images.githubusercontent.com/84647446/212552089-6b9c8b6e-a101-488b-a4f6-6a69ea513708.jpg',
     },
-    // {
-    //   id: 12,
-    //   descricao: 'Portfolio',
-    //   imgScr: '',
-    // },
+    {
+      id: 12,
+      descricao: 'Portfolio',
+      imgScr: 'https://user-images.githubusercontent.com/84647446/212552091-cc263417-5d63-46e0-aecf-5e91a49a51ae.jpg',
+    },
     {
       id: 13,
       descricao: 'Cronometro',
-      imgScr: 'https://user-images.githubusercontent.com/84647446/212436477-c1419a78-bf13-4f32-8cd2-0f87c7fa8154.jpg',
+      imgScr: 'https://user-images.githubusercontent.com/84647446/212552076-5e5baa24-526c-4845-943c-945a4a886ac2.jpg',
     },
     // {
     //   id: 14,
@@ -86,32 +86,32 @@ export default function Experience() {
     {
       id: 15,
       descricao: 'Ecommerce',
-      imgScr: 'https://user-images.githubusercontent.com/84647446/212438343-fd8b0fda-fa3a-45a9-afa7-ff33395540b7.jpg',
+      imgScr: 'https://user-images.githubusercontent.com/84647446/212552079-7f9a5a8c-71ef-423c-9457-3780c8ed0c80.jpg',
     },
     {
       id: 16,
       descricao: 'images gasolina e alcool',
-      imgScr: 'https://user-images.githubusercontent.com/84647446/212425571-83440356-636c-4629-aff2-252955909b63.jpg',
+      imgScr: 'https://user-images.githubusercontent.com/84647446/212552073-b9fb6df3-a0f3-4c25-a5f6-01bc72bd57dc.jpg',
     },
     {
       id: 17,
       descricao: 'Relógio digital',
-      imgScr: 'https://user-images.githubusercontent.com/84647446/212437155-f412427a-a0a8-4f6b-affa-c68bb9b2d711.jpg',
+      imgScr: 'https://user-images.githubusercontent.com/84647446/212552094-0b087b86-7935-46b3-80c0-f440178f0708.jpg',
     },
     // {
     //   id: 3,
     //   descricao: 'musica surpresa',
-    //   imgScr: 'https://user-images.githubusercontent.com/84647446/212430856-3f8f2afd-77db-4a40-8dce-5579bd16f5ed.jpg',
+    //   imgScr: 'https://user-images.githubusercontent.com/84647446/212552095-5fe3104c-19b2-4396-b4e8-a3ce64fbd355.jpg',
     // },
     // {
     //   id: ,
     //   descricao: 'loterica games',
-    //   imgScr: 'https://user-images.githubusercontent.com/84647446/212432209-29504bcf-c3cd-40eb-bd23-2f0dd24e093f.jpg',
+    //   imgScr: 'https://user-images.githubusercontent.com/84647446/212552086-e9198c75-f68b-453a-9c4a-4f8e9f9485ee.jpg',
     // },
     // {
     //   id: 8,
     //   descricao: 'food delivery',
-    //   imgScr: 'https://user-images.githubusercontent.com/84647446/212434775-f9b18ba0-535b-4e14-91a5-b1f94c5ccd97.jpg',
+    //   imgScr: 'https://user-images.githubusercontent.com/84647446/212552080-e7692836-0e55-407e-a3cf-ed16b7d12678.jpg',
     // },
   ];
 
